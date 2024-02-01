5e26002a0a966b15b29f0c2359149d767621b4f8
Sergio <gomez.c.sergio@outlook.com>
2024-02-01 13:51:50 +0100

    Add project files.

---
+++ Entities/Kid.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingKids.Entities
+{
+    public class Kid
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+
+        public virtual ICollection<Kid>? Siblings { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountingKids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
