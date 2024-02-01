1f6dd9ac6db42d59898c23bacde145431fc2c677
Sergio <gomez.c.sergio@outlook.com>
2024-02-01 14:08:56 +0100

    Add Price entity

---
+++ Entities/Price.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingKids.Entities
+{
+    public class Price
+    {
+        [Key]
+        public int Id {  get; set; }
+        public string Label { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
